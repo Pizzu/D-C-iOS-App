492a2311eff963360362afbb5dd9f94de2d2ee4d
Pizzu <lucaloforte3@gmail.com>
2021-03-05 18:40:49 +0100

    Initial Commit

---
+++ Shared/DCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCApp.swift
+//  Shared
+//
+//  Created by Luca Lo Forte on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct DCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
